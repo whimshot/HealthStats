5818a96d33d80f5f9036292bbc05db1e80305f82
Colin Johnson <colinj@gmail.com>
2017-07-02 17:35:56 -0400

    Refactored to use new file structure.

--- main.py
+++ main.py
@@ -4,33 +4,12 @@ from adafruitiokey import aoi_key
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
-import os
-import healthstat
+from StatsChart import StatsChart
+from FileMonkey import FileMonkey
 
 
-statschart = healthstat.StatsChart()
+statschart = StatsChart()
 statschart.draw_chart()
-
-
-class FileMonkey(object):
-    """A monkey that watches files."""
-
-    def __init__(self, filename):
-        """Make that monkey."""
-        self._cached_stamp = 0
-        self.filename = filename
-
-    def ook(self):
-        """Train that monkey."""
-        try:
-            stamp = os.stat(self.filename).st_mtime
-            if stamp != self._cached_stamp:
-                self._cached_stamp = stamp
-                return True
-            else:
-                return False
-        except ValueError:
-            return False
 
 
 class HealthStats(BoxLayout):
