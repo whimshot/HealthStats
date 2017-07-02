30f5a0d893ab66b31988cd5e362adde161351348
Colin Johnson <colinj@gmail.com>
2017-07-02 13:07:02 -0400

    Squashed commit of the following:
    commit 7ae4fef8d9391bd9fe78eecfcc31fd6fd6c4d681
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sun Jul 2 13:03:17 2017 -0400
    
        Image can now be stretched to fit.
    
    commit 2263a5d3fb2cf3f0b1709dd2acfa1cb5f48711db
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sun Jul 2 13:02:53 2017 -0400
    
        Update matplotlib to use Agg backend and not TkAgg.
    
    commit 65eb499c72d8d00116cf142da9e8fd59be6fd936
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sun Jul 2 12:59:15 2017 -0400
    
        Added polling to watch for image file changes. Only reload on change.
    
    commit 239e521e449a99252b42edccce4c6ac5c3179164
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sun Jul 2 12:31:09 2017 -0400
    
        removed unnecessary imports, rebuild chart image when data changes
    
    commit c6eaf7d7975536396978f7e3b3d62298ef5ccbcf
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sun Jul 2 12:29:47 2017 -0400
    
        removed second chart image
    
    commit 24c81a358abc2c7f7b5f1b389c4e3b01feab91af
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sun Jul 2 12:29:28 2017 -0400
    
        removed unnecessary imports and updated docstrings
    
    commit b744e90c8ad016ce7bdec07d695ad13544d713cb
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sun Jul 2 09:46:38 2017 -0400
    
        changes requirements to >=
    
    commit e2831de8945fdb03082a826d00415693f1e20206
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sun Jul 2 09:26:17 2017 -0400
    
        Update requirements files and cleand up some lint/pep8 errors.
    
    commit e3fd227c3c8a3da2d4d2740a7848ee7a61237232
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sun Jul 2 00:16:46 2017 -0400
    
        Modify the except
    
    commit 2cdc925d756ec7c79575c6ed28d9973aba63f19c
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sun Jul 2 00:03:46 2017 -0400
    
        Removed debug prints from healthstat.py.
        Put chart creation on its own schedule.
        Added matplotlib to requirements3.txt.
    
    commit 4b30299ffe2a1a07c329453c4cb576a12fca9644
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sat Jul 1 23:14:51 2017 -0400
    
        Moved the StatsChart and HealthStat classes out to their own file.
    
    commit b618fe0ff99ea6086fb98a8d71583ca44736df07
    Author: Colin Johnson <colinj@gmail.com>
    Date:   Sat Jul 1 18:48:14 2017 -0400
    
        Now with chart making and chart displaying

---
+++ healthstat.py
@@ -0,0 +1,83 @@
+"""HealthStat and StatsChart Classes."""
+from adafruitiokey import aoi_key
+from datetime import datetime
+from Adafruit_IO import Client
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+class HealthStat:
+    """HealthStats object."""
+
+    aio = Client(aoi_key)
+
+    def __init__(self, feed):
+        """Create new HealthStats object."""
+        self.feed = feed
+        self.data = []
+        self.dates = []
+
+    def __eq__(self, other):
+        """`Are they the same?` Is the question."""
+        return self.__dict__ == other.__dict__
+
+    def get_data(self):
+        """Get data from io.adatruit.com."""
+        feed_data = self.aio.data(self.feed)
+        for entry in feed_data:
+            self.data.append(entry.value)
+            date = str(entry.created_at).replace('Z', '+0000')
+            self.dates.append(datetime.strptime(date, "%Y-%m-%dT%H:%M:%S%z"))
+
+
+class StatsChart:
+    """Stats charts object."""
+
+    def __init__(self):
+        """Make that statschart."""
+        self.oldweights = HealthStat('weight')
+        self.systolics = HealthStat('systolic')
+        self.diastolics = HealthStat('diastolic')
+
+    def draw_chart(self):
+        """Make the stats chart image."""
+        try:
+            weights = HealthStat('weight')
+            systolics = HealthStat('systolic')
+            diastolics = HealthStat('diastolic')
+
+            weights.get_data()
+            systolics.get_data()
+            diastolics.get_data()
+
+            if ((weights != self.oldweights)
+                    or (systolics != self.oldsystolics)
+                    or (diastolics != self.olddiasolics)):
+
+                fig, (weight, bp) = plt.subplots(2, figsize=(4, 5.5))
+
+                bp.plot(systolics.dates, systolics.data, 'bo')
+                bp.plot(diastolics.dates, diastolics.data, 'go')
+                bp.set_ylabel('Systolic & Diasolic')
+
+                weight.plot(weights.dates, weights.data)
+                weight.set_ylabel('Kg')
+                weight.set_ylim(120, 144)
+
+                for ax in fig.axes:
+                    matplotlib.pyplot.sca(ax)
+                    plt.xticks(rotation=45)
+
+                fig.tight_layout()
+                fig.savefig('weight.png', dpi=300)
+
+                plt.clf()
+
+        except ValueError as ve:
+            print("somethings not right %s" % str(ve))
+
+        finally:
+            self.oldweights = weights
+            self.oldsystolics = systolics
+            self.olddiasolics = diastolics
--- main.py
+++ main.py
@@ -1,57 +1,83 @@
+"""Health Stats app in kivy."""
+from Adafruit_IO import Client
+from adafruitiokey import aoi_key
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
-from Adafruit_IO import Client
-import time
-from adafruitiokey import aoi_key
+import os
+import healthstat
+
+
+statschart = healthstat.StatsChart()
+statschart.draw_chart()
+
+
+class FileMonkey(object):
+    """A monkey that watches files."""
+
+    def __init__(self, filename):
+        """Make that monkey."""
+        self._cached_stamp = 0
+        self.filename = filename
+
+    def ook(self):
+        """Train that monkey."""
+        try:
+            stamp = os.stat(self.filename).st_mtime
+            if stamp != self._cached_stamp:
+                self._cached_stamp = stamp
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
 
 
 class HealthStats(BoxLayout):
     """A simple class for a Health Stats app in kivy."""
 
-    aio = Client(aoi_key)
-    aio_feed = 'weight'
     screen_text = "Health Stats"
+    aio = Client(aoi_key)
+    fm = FileMonkey('weight.png')
 
     def update(self, dt):
+        """Update the display and charts."""
         self.inputpad.numscreen.text = self.screen_text
+        if (self.fm.ook()):
+            self.weightchart.reload()
+
+    def update_charts(self, dt):
+        """Make the charts."""
+        statschart.draw_chart()
 
     def new_digit(self, text):
-        """Adds a digit or decimal point to the input display"""
-
+        """Add a digit or decimal point to the input display."""
         if (self.screen_text == "Health Stats"):
             self.screen_text = text
         else:
             self.screen_text = self.screen_text + text
 
     def key_function(self, name):
-        """handles the function keys"""
-
+        """Handle the function keys."""
         try:
             vital_stat = float(self.screen_text)
-
-            if  (name == "delete"):
+            if (name == "delete"):
                 self.screen_text = self.screen_text[:-1]
-
             elif (name == "weight"):
-                date = time.strftime("%Y-%m-%d %H:%M:%S %z")
                 self.screen_text = "Health Stats"
                 self.aio.send(name, vital_stat)
                 bmi = int(vital_stat/3.161284)
                 self.aio.send('bmi', bmi)
 
             elif (name == "systolic"):
-                date = time.strftime("%Y-%m-%d %H:%M:%S %z")
                 self.screen_text = "Health Stats"
                 self.aio.send(name, vital_stat)
 
             elif (name == "diastolic"):
-                date = time.strftime("%Y-%m-%d %H:%M:%S %z")
                 self.screen_text = "Health Stats"
                 self.aio.send(name, vital_stat)
 
             elif (name == "pulse"):
-                date = time.strftime("%Y-%m-%d %H:%M:%S %z")
                 self.screen_text = "Health Stats"
                 self.aio.send(name, vital_stat)
 
@@ -62,10 +88,14 @@ class HealthStats(BoxLayout):
 
 
 class HealthStatsApp(App):
+    """Kivy App Class for Health Stats."""
 
     def build(self):
+        """Build function for Health Stats kivy app."""
         hs = HealthStats()
         Clock.schedule_interval(hs.update, 1.0 / 10.0)
+        Clock.schedule_interval(hs.update_charts, 5.0)
+
         return hs
 
 
