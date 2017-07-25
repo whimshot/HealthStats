538390fd2579bf0ab9c3814c7456f34fb8e9a78a
Colin Johnson <colinj@gmail.com>
2017-07-24 20:22:29 -0400

    Don't need this file.

--- HSButtons.py
+++
@@ -1,8 +0,0 @@
-"""Health Stats buttons."""
-from HSConfig import config
-from kivy.uix.button import Button
-from HSLogger import logger
-
-
-class DigitKey(Button):
-    """A button that holds a digit."""
