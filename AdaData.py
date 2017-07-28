bc336c08761dd1d63d7251fb0045daa937e6f5e3
Colin Johnson <colinj@gmail.com>
2017-07-27 23:35:06 -0400

    just for giggles

--- AdaData.py
+++ AdaData.py
@@ -3,15 +3,16 @@ import logging
 import logging.handlers
 from datetime import datetime
 
-from Adafruit_IO import Client
+from Adafruit_IO import Client, MQTTClient
 from dateutil import tz
 from HSConfig import config
 from HSLogger import HostnameFilter
 
 AIO_KEY = config.get('Adafruit', 'aio_key')
+AIO_ID = config.get('Adafruit', 'aio_id')
 
 
-class AdaData(object):
+class AdaFeed(object):
     """AdaData object."""
 
     aio = Client(AIO_KEY)
@@ -27,13 +28,19 @@ class AdaData(object):
             self.feed = feed
             self.data = []
             self.dates = []
-            self.logger.info('AdaData instance for {0}.'.format(feed))
+            self.client = MQTTClient(AIO_ID, AIO_KEY)
+            self.client.on_connect = self.connected
+            self.client.on_disconnect = self.disconnected
+            self.client.on_message = self.message
+            self.client.connect()
+            self.logger.debug('Data instance for {0}.'.format(feed))
+            self.client.loop_background()
         except Exception:
-            self.logger.exception('Failed to instantiate AdaData.')
+            self.logger.exception('Failed to instantiate AdaFeed.')
 
     def __eq__(self, other):
         """`Are they the same?` Is the question."""
-        self.logger.debug('Comparing two instances of AdaData.')
+        self.logger.debug('Comparing two instances of AdaFeed.')
         return self.__dict__ == other.__dict__
 
     def get_data(self):
@@ -52,6 +59,114 @@ class AdaData(object):
                 local = utc.astimezone(self.to_zone)
                 self.dates.append(local)
                 self.logger.debug('Appended {0}'.format(local))
-            self.logger.info('Retrieved data from {0}.'.format(self.feed))
+            self.logger.debug('Retrieved data from {0}.'.format(self.feed))
         except Exception:
             self.logger.exception('Caught exception.')
+
+    def connected(self, client):
+        """Called when connection to io.adafruit.com is successful."""
+        # Subscribe to changes for the feeds listed.
+        self.logger.debug('Connected to Adafruit, subscribing to feeds.')
+        try:
+            self.logger.debug('Subscribing to {0} feed.'.format(self.feed))
+            self.client.subscribe(self.feed)
+        except Exception:
+            self.logger.exception('Failed to subscribe to feed.')
+
+    def disconnected(self, client):
+        """Called when disconnected from io.adafruit.com."""
+        self.logger.info('Disconnected from Adafruit IO!')
+
+    def message(self, client, feed_id, payload):
+        """Called when a subscribed feed gets new data."""
+        try:
+            self.logger.debug("Feed: {0} received".format(feed_id)
+                              + " new data: {0}".format(payload))
+            self.get_data()
+        except Exception:
+            raise
+        finally:
+            pass
+
+
+class AdaData():
+    """This is where we store the data from all the feeds."""
+
+    def __init__(self):
+        """
+        Initial AdaData.
+
+        Sets up initial containers for data from io.adafruit.com to
+        compare against for changes later.
+        """
+        try:
+            self.feeds = ['weight', 'diastolic', 'systolic', 'pulse', 'bmi']
+            self.feeds_data = {}     # The data last retrieved from the feeds.
+            self.filename = ""
+            self.logger = \
+                logging.getLogger('HealthStats.'
+                                  + self.__class__.__name__)
+            self.logger.addFilter(HostnameFilter())
+            self.logger.info('creating an instance of ChartMaker')
+            self.client = MQTTClient(AIO_ID, AIO_KEY)
+            self.client.on_connect = self.connected
+            self.client.on_disconnect = self.disconnected
+            self.client.on_message = self.message
+
+            for feed in self.feeds:
+                self.feeds_data[feed] = AdaFeed(feed)
+                self.logger.debug("Getting data from io.adatfruit.com"
+                                  + " for {0}.".format(feed))
+                self.feeds_data[feed].get_data()
+
+            self.client.connect()
+            self.logger.debug('Getting AdaData instance up and running.')
+            self.client.loop_background()
+        except Exception:
+            self.logger.exception('AdaData instantiation failed.')
+
+    def get_data(self):
+        try:
+            for feed in self.feeds:
+                self.feeds_data[feed].get_data()
+        except Exception:
+            raise
+        finally:
+            pass
+
+    def connected(self, client):
+        """Called when connection to io.adafruit.com is successful."""
+        # Subscribe to changes for the feeds listed.
+        self.logger.debug('Connected to Adafruit, subscribing to feeds.')
+        try:
+            for feed in self.feeds:
+                self.logger.debug('Subscribing to {0} feed.'.format(feed))
+                self.client.subscribe(feed)
+        except Exception:
+            self.logger.exception('Failed to subscribe to feed.')
+
+    def disconnected(self, client):
+        """Called when disconnected from io.adafruit.com."""
+        self.logger.info('Disconnected from Adafruit IO!')
+
+    def message(self, client, feed_id, payload):
+        """Called when a subscribed feed gets new data."""
+        try:
+            self.logger.debug("Feed: {0} received".format(feed_id)
+                              + " new data: {0}".format(payload))
+            self.feeds_data[feed_id].get_data()
+            if (feed_id in ('weight', 'bmi')):
+                self.weight_chart()
+            if (feed_id in ('systolic', 'diastolic', 'pulse')):
+                self.bp_chart()
+            if (feed_id in ('systolic', 'diastolic', 'pulse',
+                            'weight', 'bmi')):
+                self.small_charts()
+        except Exception:
+            raise
+        finally:
+            pass
+
+
+if __name__ == '__main__':
+    ad = AdaData()
--- Chart.py
+++ Chart.py
@@ -18,7 +18,7 @@ from HSLogger import HostnameFilter, logger
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.image import Image
-from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 
 matplotlib.use('module://kivy.garden.matplotlib.backend_kivyagg')
 import matplotlib.pyplot as plt  # noqa
@@ -37,12 +37,12 @@ matplotlib.rc('legend', framealpha=0.5, loc='best')
 # Set matplotlib global linewidth
 matplotlib.rcParams['axes.linewidth'] = 0.5
 
+feeds_data = AdaData()
+
 
 class ChartMaker():
     """Stats charts object."""
 
-    feeds = ['weight', 'diastolic', 'systolic', 'pulse', 'bmi']
-    feeds_data = {}     # The data last retrieved from the feeds.
     filename = ""
 
     def __init__(self):
@@ -58,76 +58,29 @@ class ChartMaker():
                                   + self.__class__.__name__)
             self.logger.addFilter(HostnameFilter())
             self.logger.info('creating an instance of ChartMaker')
-            self.client = MQTTClient(AIO_ID, AIO_KEY)
-            self.client.on_connect = self.connected
-            self.client.on_disconnect = self.disconnected
-            self.client.on_message = self.message
-
-            for feed in self.feeds:
-                self.feeds_data[feed] = AdaData(feed)
-                self.logger.debug("Getting data from io.adatfruit.com"
-                                  + " for {0}.".format(feed))
-                self.feeds_data[feed].get_data()
-
-            self.client.connect()
-            self.logger.info('Getting ChartMaker instance up and running.')
-            self.client.loop_background()
         except Exception:
             self.logger.exception('ChartMaker instantiation failed.')
 
-    def connected(self, client):
-        """Called when connection to io.adafruit.com is successful."""
-        # Subscribe to changes for the feeds listed.
-        self.logger.debug('Connected to Adafruit, subscribing to feeds.')
-        try:
-            for feed in self.feeds:
-                self.logger.debug('Subscribing to {0} feed.'.format(feed))
-                self.client.subscribe(feed)
-        except Exception:
-            self.logger.exception('Failed to subscribe to feed.')
-
-    def disconnected(self, client):
-        """Called when disconnected from io.adafruit.com."""
-        self.logger.info('Disconnected from Adafruit IO!')
-
-    def message(self, client, feed_id, payload):
-        """Called when a subscribed feed gets new data."""
-        try:
-            self.logger.info("Feed: {0} received".format(feed_id)
-                             + " new data: {0}".format(payload))
-            self.feeds_data[feed_id].get_data()
-            if (feed_id in ('weight', 'bmi')):
-                self.weight_chart()
-            if (feed_id in ('systolic', 'diastolic', 'pulse')):
-                self.bp_chart()
-            if (feed_id in ('systolic', 'diastolic', 'pulse',
-                            'weight', 'bmi')):
-                self.small_charts()
-        except Exception:
-            raise
-        finally:
-            pass
-
-    def small_charts(self):
+    def small_charts():
         """Make the stats chart image."""
         try:
             fig, (weight_chart, bp_chart) = plt.subplots(2, figsize=(4, 4.8))
             bmi_major_locator = MultipleLocator(1)
 
-            bp_chart.plot(self.feeds_data['systolic'].dates,
-                          self.feeds_data['systolic'].data,
+            bp_chart.plot(feeds_data['systolic'].dates,
+                          feeds_data['systolic'].data,
                           label='Systolic')
-            bp_chart.plot(self.feeds_data['diastolic'].dates,
-                          self.feeds_data['diastolic'].data,
+            bp_chart.plot(feeds_data['diastolic'].dates,
+                          feeds_data['diastolic'].data,
                           label='Diastolic')
-            bp_chart.plot(self.feeds_data['pulse'].dates,
-                          self.feeds_data['pulse'].data,
+            bp_chart.plot(feeds_data['pulse'].dates,
+                          feeds_data['pulse'].data,
                           label='Pulse')
             bp_chart.set_ylabel('Blood Pressure (mmHg)\nPulse (BPM)')
             bp_chart.tick_params(axis='y')
 
-            weight_chart.plot(self.feeds_data['weight'].dates,
-                              self.feeds_data['weight'].data, 'C0')
+            weight_chart.plot(feeds_data['weight'].dates,
+                              feeds_data['weight'].data, 'C0')
             weight_chart.set_ylabel('Weight (Kg)',
                                     fontsize='9',
                                     color='C0')
@@ -136,8 +89,8 @@ class ChartMaker():
 
             bmi_chart = weight_chart.twinx()
             bmi_chart.yaxis.set_major_locator(bmi_major_locator)
-            bmi_chart.plot(self.feeds_data['bmi'].dates,
-                           self.feeds_data['bmi'].data, 'C1')
+            bmi_chart.plot(feeds_data['bmi'].dates,
+                           feeds_data['bmi'].data, 'C1')
             bmi_chart.set_ylabel('BMI',
                                  fontsize='9',
                                  color='C1')
@@ -151,27 +104,30 @@ class ChartMaker():
                                labelsize='8')
                 ax.spines['top'].set_visible(False)
             fig.tight_layout()
+            canvas = fig.canvas
+            canvas.draw()
             fig.savefig('SmallCharts.png', dpi=100)
-
+            canvas = fig.canvas
+            canvas.draw()
             plt.clf()
-
+            return canvas
         except Exception:
-            self.logger.exception('Failed to draw new charts.')
+            pass
 
-    def bp_chart(self):
+    def bp_chart():
         """Make the stats chart image."""
         try:
             bp_minor_locator = MultipleLocator(2)
             fig, bp_chart = plt.subplots(1, figsize=(8, 4.8))
             bp_chart.yaxis.set_minor_locator(bp_minor_locator)
-            bp_chart.plot(self.feeds_data['systolic'].dates,
-                          self.feeds_data['systolic'].data,
+            bp_chart.plot(feeds_data['systolic'].dates,
+                          feeds_data['systolic'].data,
                           label='Systolic')
-            bp_chart.plot(self.feeds_data['diastolic'].dates,
-                          self.feeds_data['diastolic'].data,
+            bp_chart.plot(feeds_data['diastolic'].dates,
+                          feeds_data['diastolic'].data,
                           label='Diastolic')
-            bp_chart.plot(self.feeds_data['pulse'].dates,
-                          self.feeds_data['pulse'].data,
+            bp_chart.plot(feeds_data['pulse'].dates,
+                          feeds_data['pulse'].data,
                           label='Pulse')
             bp_chart.grid(which='major')
             bp_chart.yaxis.grid(which='minor')
@@ -192,17 +148,17 @@ class ChartMaker():
             return canvas
 
         except Exception:
-            self.logger.exception('Failed to draw new charts.')
+            pass
 
-    def weight_chart(self):
+    def weight_chart():
         """Make the stats chart image."""
         try:
             fig, weight_chart = plt.subplots(1, figsize=(8, 4.8))
             weight_minor_locator = MultipleLocator(.2)
             bmi_major_locator = MultipleLocator(1)
             weight_chart.yaxis.set_minor_locator(weight_minor_locator)
-            wc = weight_chart.plot(self.feeds_data['weight'].dates,
-                                   self.feeds_data['weight'].data,
+            wc = weight_chart.plot(feeds_data['weight'].dates,
+                                   feeds_data['weight'].data,
                                    'C0', label='Weight (Kg)')
             weight_chart.set_ylabel('Weight (Kg)',
                                     fontsize='9',
@@ -215,8 +171,8 @@ class ChartMaker():
 
             bmi_chart = weight_chart.twinx()
             bmi_chart.yaxis.set_major_locator(bmi_major_locator)
-            bc = bmi_chart.plot(self.feeds_data['bmi'].dates,
-                                self.feeds_data['bmi'].data,
+            bc = bmi_chart.plot(feeds_data['bmi'].dates,
+                                feeds_data['bmi'].data,
                                 'C1', label='BMI')
             bmi_chart.set_ylabel('BMI',
                                  fontsize='9',
@@ -237,28 +193,18 @@ class ChartMaker():
             fig.tight_layout()
             canvas = fig.canvas
             canvas.draw()
-
             plt.clf()
-
             return canvas
         except Exception:
-            self.logger.exception('Failed to draw new charts.')
-
-    def test(self):
-        """Test ChartMaker by building each of the charts."""
-        print("Start:\t%s" % time.ctime())
-        self.weight_chart()
-        self.bp_chart()
-        self.small_charts()
-        print("Finish:\t%s" % time.ctime())
+            # self.logger.exception('Failed to draw new charts.')
+            pass
 
 
-class Chart(Image):
+chart_maker = ChartMaker
+
+
+class Chart(BoxLayout):
     """The basic Chart class we will build off from here."""
-
-    feeds = []          # The list of feeds the chart is subscribed to.
-    filename = ""
-    chartmaker = ChartMaker()
 
     def __init__(self, **kwargs):
         """Chart object instance."""
@@ -269,56 +215,19 @@ class Chart(Image):
                                   + self.__class__.__name__)
             self.logger.addFilter(HostnameFilter())
             self.logger.info("Creating an instance of " + __name__)
+            feeds_data.client.on_message = self.message()
         except Exception:
             self.logger.exception('Chart instantiation failed.')
         finally:
             pass
 
-    def check_chart(self, dt):
-        """Check to see if the image has changed and needs to be reloaded."""
-        try:
-            if (self.fm.ook()):
-                self.reload()
-        except Exception:
-            logger.exception("Caught exception.")
-        finally:
-            pass
-
-    def build(self):
-        """Builder for chart object."""
-        try:
-            self.filename = str(self.source)
-            self.fm = FileMonkey(self.filename)
-            Clock.schedule_interval(self.check_chart, 5)
-            logger.debug("Building new chart {0}.".format(self.filename))
-        except Exception:
-            logger.exception("Caught exception.")
-        finally:
-            pass
-
-    def connected(self, client):
-        """Called when connection to io.adafruit.com is successful."""
-        # Subscribe to changes for the feeds listed.
-        self.logger.info('Connected to Adafruit, subscribing to feeds.')
-        try:
-            for feed in self.feeds:
-                self.logger.debug('Subscribing to {0} feed.'.format(feed))
-                self.client.subscribe(feed)
-        except Exception:
-            self.logger.exception('Failed to subscribe to feed.')
-
-    def disconnected(self, client):
-        """Called when disconnected from io.adafruit.com."""
-        self.logger.info('Disconnected from Adafruit IO!')
-
-    def message(self, client, feed_id, payload):
+    def message(self):
         """Called when a subscribed feed gets new data."""
         try:
-            self.logger.debug("Feed: {0} received new data: ".format(feed_id)
-                              + "{0}".format(payload))
-            dummy_event = threading.Event()
-            dummy_event.wait(timeout=5)
-            self.reload()
+            # self.logger.debug("Feed: {0} received".format(feed_id)
+            #                   + " new data: {0}".format(payload))
+            # feeds_data[feed_id].get_data()
+            self.canvas.draw
         except Exception:
             raise
         finally:
@@ -328,11 +237,15 @@ class Chart(Image):
 class WeightChart(Chart):
     """Class for the WeightChart image."""
 
+    canvas = chart_maker.weight_chart()
+
     pass
 
 
 class BPChart(Chart):
     """Class for the BPChart image."""
+
+    canvas = chart_maker.bp_chart()
 
     pass
 
@@ -340,10 +253,12 @@ class BPChart(Chart):
 class SmallCharts(Chart):
     """Class for the SmallCharts image."""
 
+    canvas = chart_maker.small_charts()
+
     pass
 
 
-class ChartDemo(Widget):
+class ChartDemo(BoxLayout):
     """Demo for testing charts."""
 
 
@@ -354,12 +269,8 @@ class ChartApp(App):
         """Build function for ChartDemo kivy app."""
         logger.info('Starting ChartDemoApp.')
         cd = ChartDemo()
-        cd.weightchart.build()
-        cd.bpchart.build()
-        cd.smallcharts.build()
         return cd
 
 
 if __name__ == '__main__':
     ChartApp().run()
-    ChartMaker().test()
